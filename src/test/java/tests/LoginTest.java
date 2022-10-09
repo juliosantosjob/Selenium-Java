619b91b9e90cce0372db751f2b7b21d126dd44df
Julio Santos <julioc.9730@gmail.com>
2022-10-09 10:20:23 -0300

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import pages.LoginPage;
-import static org.junit.Assert.assertTrue;
+import org.testing.Assert;
 import static org.testng.AssertJUnit.assertEquals;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
