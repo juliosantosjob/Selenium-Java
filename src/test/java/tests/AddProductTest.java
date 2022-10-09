0c452af06840c8a2b17051d3a71c0361825d0372
Julio Santos <julioc.9730@gmail.com>
2022-10-09 00:01:37 -0300

    Update AddProductTest.java

--- src/test/java/tests/AddProductTest.java
+++ src/test/java/tests/AddProductTest.java
@@ -9,6 +9,7 @@ import pages.AddProductPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AddProductTest {
 	WebDriver driver;
